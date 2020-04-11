23dccd6bbe928ef8bfc8bdcf04a34eff4a1cbb91
Andrew Van Deventer <vandeventerandy@gmail.com>
2020-04-11 19:02:28 -0400

    Committing progress to a stackblitz repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Post {
+  title: string;
+  content: string;
+}
+
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
-  name = 'Angular';
+  title = 'Nowhere';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { PlayerManagerComponent } from './player-manager/player-manager.component';
+import { MapComponent } from './map/map.component';
+import { LocationComponent } from './location/location.component';
+import { EncounterComponent } from './encounter/encounter.component';
+import { OptionComponent } from './option/option.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FormsModule,
+    RouterModule.forRoot([
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    PlayerManagerComponent,
+    LocationComponent,
+    EncounterComponent,
+    OptionComponent,
+    MapComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
